068bd8ec40a7f10f774c45641d4fd5a987b9dacd
marentropico <marcosfigueroa.br@gmail.com>
2026-05-20 20:28:23 -0300

    Finalizamos o projeto

--- assets/script.js
+++ assets/script.js
@@ -20,3 +20,22 @@ if(container) {
         container.appendChild(div);
     }
 }
+
+const form = document.getElementById('formulario-adesao')
+const msg = document.getElementById('mensagem-retorno')
+
+if(form) {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+
+        if( form.checkValidity()) {
+            const nome = document.getElementById('nome').value;
+            msg.textContent = `✅ Sucesso, ${nome}! Adesão registrada.`;
+            msg.className = 'mensagem-retorno sucesso';
+        } else {
+            msg.textContent = '❌ Por favor, preencha todos os campos corretamente.'
+            msg.className = 'mensagem-retorno erro';
+            msg.style.display = 'block';
+        }
+    })
+}
